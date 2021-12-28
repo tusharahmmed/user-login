d2245a207f915d6e45c8e3245c8d3f649b0b466e
tusharahmmed <sakibahmmedtushar@gmail.com>
2021-12-28 18:18:06 +0600

    use env variable

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -1,11 +1,11 @@
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAapxtNRrtM3J5J3yIfezNwT8rjnM4fl5U",
-  authDomain: "user-login-system-management.firebaseapp.com",
-  projectId: "user-login-system-management",
-  storageBucket: "user-login-system-management.appspot.com",
-  messagingSenderId: "1062182926497",
-  appId: "1:1062182926497:web:8fac4506b1eac8156958e0"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STRONGE_BUCKED,
+  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebaseConfig;
